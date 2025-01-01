16bb42f8acb17984d7ae2a50b02675f7b184edea
lichuang <chuang.li@raisethink.com>
2025-01-01 20:32:19 +0800

    比较器调整

--- CompareBuilder.js
+++ CompareBuilder.js
@@ -42,12 +42,17 @@ module.exports = (function () {
         //对象比较
         __objectCompare(v1, v2, compareLogic) {
             let keys = this.__getKeys(v1, v2);
-            let compareResult = {};
+            let compareInfo = {};
+            let compareStatus = true;
             for(let attribute of keys) {
                 let result = this.compareCode(v1[attribute], v2[attribute], compareLogic);
-                compareResult[attribute] = result;
+                compareInfo[attribute] = result;
+                compareStatus = compareStatus && this.parseCompareStatus(result);
             }
-            return compareResult;
+            return {
+                compareInfo: compareInfo,
+                compare: compareStatus
+            };
         }
         //日期比较
         __dataCompare(v1, v2) {
@@ -64,14 +69,35 @@ module.exports = (function () {
                 let length = v1.length;
                 for (let index = 0; index < length; index++) {
                     let compare = this.compareCode(v1[index], v2[index], compareLogic);
-                    if (compare.compare === true) {
-                        
+                    let compareStatus = this.parseCompareStatus(compare);
+                    if (!compareStatus) {
+                        return compareStatus;
                     }
                 }
                 return true;
             } else {
                 return false;
             }
+        }
+        parseCompareStatus (compare) {
+            if (TypeUtils.parseType(compare) === 'boolean') {
+                return compare;
+            }
+            if(TypeUtils.parseType(compare) === 'object'){
+                let compareStatus = this.parseCompareStatus(compare.compare);
+                return compareStatus;
+                // if (!compareStatus) {
+                //     return compareStatus;
+                // }
+                //遍历比较结果，如果所有的比较结果都是true(也就是相同)返回True, 如果存在一个false,结束遍历，返回false
+                // for (let attribute of compare) {
+                //     let compareStatus = this.__parseCompareStatus(compare[attribute].compare);
+                    
+                // }
+                // return true;
+            }
+            //如果compare 不是对象，也不是Boolean 类型，直接返回False;
+            return false;
         }
 
         __getKeys(v1, v2) {
@@ -106,16 +132,21 @@ module.exports = (function () {
                  * 两个数据类型相等，且需要更加精细的比较的情况
                  * JSON 字符串(string) -- JSON 字符串(string)
                  */
-                compare = this.bulider(t1)(v1, v2, compareLogic);
+                compare = this.bulider(t1).call(this, v1, v2, compareLogic);
             }
-            return {
-                _t1: t1,
-                _t2: t2,
-                _v1: v1,
-                _v2: v2,
-                compare: compare,
+            if (t1 === 'object' && t2 === 'object') {
+                return compare;
+            }else{
+                return {
+                    compareInfo: {
+                        _t1: t1,
+                        _t2: t2,
+                        _v1: v1,
+                        _v2: v2,
+                    },
+                    compare: compare,
+                }
             }
-
         }
     }
 }());
